9aa48090280a0a38b76fde2e1c172de16aabb21c
Temitayo Fagbami <tefagbam@microsoft.com>
2020-07-29 08:25:58 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>React Weather App</h1>
+    <main>
+        {/* add weather fetching component */}
+      </main>
+      <footer>
+        Page created by Tayo
+      </footer>
+    </div>
+  );
+}
+
+export default App;
